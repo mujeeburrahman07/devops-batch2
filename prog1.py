515cf4f6d135571d6b7e5e5676611a4e85ba2b28
Sai <shashisk123@gmail.com>
2018-08-28 16:35:58 -0400

    conflict2

--- prog1.py
+++ prog1.py
@@ -1,6 +1,11 @@
 print("Hello All")
 print("hello again")
+jkngkjsngjsngsjg
 print("hello 2")
+jskgbkjs
+jksbgksjgbjskg
 print("Hey.. I am back again")
-print("Hey i am sai ")
+print("Hey i am sai ")jkbgksg
+kjsbgksjbg
+ksjgbsdng
 the changes are made by me.
